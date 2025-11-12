24b81441e75df6b2e6cbb727ada1c47655f79cd8
a25136sh <a25136sh@aiit.ac.jp>
2025-11-12 15:58:25 +0900

    add rec text

--- src/views/TrialView.vue
+++ src/views/TrialView.vue
@@ -91,6 +91,23 @@ const micOff = () => {
         <Check @click="micOff" />
       </el-icon>
     </el-button>
+    <div class="recording" v-if="recording">●REC</div>
     <ProgressBox />
   </div>
 </template>
+
+<style lang="css" scoped>
+.recording {
+  color: red;
+  animation: blink 1s ease-in-out infinite alternate;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
