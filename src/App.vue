22421a9abdaf17c9cfaf7fabab4e59138bf50a10
a25136sh <a25136sh@aiit.ac.jp>
2025-11-13 23:57:45 +0900

    add login form

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
 import { RouterView } from 'vue-router'
 import { UserFilled } from '@element-plus/icons-vue'
 
 import router from '@/router'
 import { useUserStore } from './stores/user'
+import { ElMessage } from 'element-plus'
 
 const userStore = useUserStore()
+
+const userTab = ref(false)
+const form = reactive({
+  id: '',
+  name: '',
+  password: '',
+})
 
 const backHome = () => {
   router.push('/')
 }
+const onSubmit = () => {
+  userTab.value = false
+  ElMessage.success({
+    message: 'ログイン完了',
+  })
+  userStore.userId = Number(form.id)
+  userStore.username = form.name
+}
+const cancel = () => {
+  form.id = ''
+  form.name = ''
+  form.password = ''
+  userTab.value = false
+}
 </script>
 
 <template>
-  <header>
+  <header class="et_header">
     <div class="title" @click="backHome">
       <img class="logo" src="@/assets/logo.png" />
       <span class="wdxl-lubrifont-jp-n-regular">専門職英会話</span>
       <span class="mochiy-pop-p-one-regular" style="margin-top: -6px">トレーナー</span>
     </div>
-    <div class="user">
+    <div class="user" @click="userTab = true" style="cursor: pointer">
       <span class="username">{{ userStore.username }}</span>
       <el-avatar :icon="UserFilled" />
     </div>
@@ -28,7 +51,7 @@ const backHome = () => {
   <main>
     <RouterView />
   </main>
-  <footer>
+  <footer class="et_footer">
     <div><b>Group 3</b></div>
     <div>
       <a href="https://github.com/a25136sh/englishtrainer"
@@ -41,4 +64,32 @@ const backHome = () => {
       >
     </div>
   </footer>
+  <el-drawer v-model="userTab" title="アカウント情報">
+    <div v-if="userStore.username == 'guest'">
+      <span>あなたは現在ログインしていません。</span>
+      <el-form :model="form" label-width="auto" style="max-width: 600px; margin-top: 2em">
+        <el-form-item label="ユーザーID">
+          <el-input v-model="form.id" />
+        </el-form-item>
+        <el-form-item label="ユーザー名">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="パスワード">
+          <el-input type="password" v-model="form.password" />
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="onSubmit"
+            :disabled="form.id == '' || form.name == '' || form.password == ''"
+            >ログイン</el-button
+          >
+          <el-button @click="cancel">キャンセル</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <span v-else
+      >あなたは現在 <b>{{ userStore.username }}</b> としてログインしています。</span
+    >
+  </el-drawer>
 </template>
